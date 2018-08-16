833b3da0709dd06a57be135158d88b384d3ac361
unknown <sushilkumar4sep@gmail.com>
2018-08-16 16:36:46 +0530

    set watch to false

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  watch: true,
+  watch: false,
   resolve: {
     extensions: ['.js', '.scss'],
   },
